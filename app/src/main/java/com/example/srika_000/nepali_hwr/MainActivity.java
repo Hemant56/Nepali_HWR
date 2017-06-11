c2634c1d4065c97344c342f3d3899b89468f5bed
ram <shivramshrestha01@gmail.com>
2017-06-11 11:30:22 +0545

    aaa

--- app/src/main/java/com/example/srika_000/nepali_hwr/MainActivity.java
+++ app/src/main/java/com/example/srika_000/nepali_hwr/MainActivity.java
@@ -168,7 +168,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
