323b7e1e133d37a31d02c06e3e18aba3d0fcc1d0
ram <shivramshrestha01@gmail.com>
2017-07-03 15:19:22 +0545

    Written in OCR Changes

--- app/src/main/java/com/example/srika_000/nepali_hwr/Offline_OCR.java
+++ app/src/main/java/com/example/srika_000/nepali_hwr/Offline_OCR.java
@@ -14,6 +14,7 @@ import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -24,8 +25,7 @@ public class Offline_OCR extends AppCompatActivity {
     SurfaceView cameraview;
     TextView textView;
 
-
-    
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
